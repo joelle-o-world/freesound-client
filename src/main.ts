81618207b43920f552ca4f1a2d8de57963d5253e
Joelle Plow <joelleplow@pm.me>
2022-07-17 12:00:33 +0100

    new command: `freesound play`

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import YAML from "yaml";
 import { RCFile } from "./rw-rc";
 import { resolve } from "path";
 import { createWriteStream } from "fs";
+import createPlayer from "play-sound";
 
 const command = process.argv[2];
 
@@ -12,6 +13,20 @@ const rcfile = new RCFile("freesound");
 
 (async function main() {
   const freesound = await login();
+
+  async function download(soundId: string) {
+    const { suggestedFilename, stream } = await freesound.download(soundId);
+    const saveDir = await rcfile.askAndStore("saveLocation");
+    const savePath = resolve(saveDir, suggestedFilename);
+    const writer = createWriteStream(savePath);
+    stream.pipe(writer);
+    await new Promise<void>((fulfil, reject) => {
+      writer.on("close", () => fulfil());
+      writer.on("error", (err) => reject(err));
+    });
+
+    return savePath;
+  }
 
   const subArgs = process.argv.slice(3);
   switch (command) {
@@ -41,16 +56,16 @@ const rcfile = new RCFile("freesound");
       break;
 
     case "download":
-      const { suggestedFilename, stream } = await freesound.download(
-        subArgs[0]
-      );
-      const saveDir = await rcfile.askAndStore("saveLocation");
-      const savePath = resolve(saveDir, suggestedFilename);
-      const writer = createWriteStream(savePath);
-      stream.pipe(writer);
+      console.log(await download(subArgs[0]));
 
-      console.log(savePath);
+      break;
 
+    case "play":
+      const savePath = await download(subArgs[0]);
+      const player = createPlayer();
+      player.play(savePath, (err) => {
+        console.log("done!");
+      });
       break;
 
     default:
