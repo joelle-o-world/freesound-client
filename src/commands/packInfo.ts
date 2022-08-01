e01d2fc72d9c38eff36240b0d165fb668d81be32
Joelle Plow <joelleplow@pm.me>
2022-08-02 00:19:06 +0100

    Experiment with sub-sub-commands

--- src/commands/packInfo.ts
+++ src/commands/packInfo.ts
@@ -2,18 +2,8 @@ import { login } from "../connect";
 import YAML from "yaml";
 import { Command } from "commander";
 
-export const pack = new Command()
-  .name("pack")
-  .argument("<pack-id>")
-  .description("Get metadata about a sample pack")
-  .action(async (packId) => {
-    const freesound = await login();
-    const response = await freesound.packInfo(packId);
-    console.log(YAML.stringify(response.data));
-  });
-
-export const packList = new Command()
-  .name("pack-list")
+const packList = new Command()
+  .name("list")
   .description("List all the sounds in a sample pack")
   .argument("<pack-id>")
   .action(async (packId) => {
@@ -23,3 +13,17 @@ export const packList = new Command()
     for await (const sound of sounds)
       console.log(`${sound.id} - ${sound.name}`);
   });
+
+const packInfo = new Command()
+  .name("info")
+  .argument("<pack-id>")
+  .description("Get metadata about a sample pack")
+  .action(async (packId) => {
+    const freesound = await login();
+    const response = await freesound.packInfo(packId);
+    console.log(YAML.stringify(response.data));
+  });
+
+export const pack = new Command("pack")
+  .addCommand(packList)
+  .addCommand(packInfo);
