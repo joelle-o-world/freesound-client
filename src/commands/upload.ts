96c5888fa6bc1d1e5aefe4d7077ebeb30f14e898
Joelle Plow <joelleplow@pm.me>
2022-08-02 16:02:48 +0100

    WIP command: freesound upload pack <dir>

--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -1,10 +1,57 @@
 import { login } from "../connect";
-import path, { basename, resolve } from "path";
+import path, { basename, dirname, resolve } from "path";
 import { createReadStream } from "fs";
 import { Command } from "commander";
 import { filenameTags, parseCommaSeparated, topUpTags } from "../tags";
+import glob from "glob-promise";
 
 // TODO: --metadata <file> option
+
+export const uploadPack = new Command()
+  .name("pack")
+  .description("Upload a folder as a sample pack")
+  .argument(
+    "<folder>",
+    "Path to the directory containing the samples of your pack"
+  )
+  .option(
+    "-t|--tags <comma-separated-list>",
+    "Comma-separated-list of tags to attach to the uploaded sounds"
+  )
+  .option(
+    "--description <string>",
+    "Describe your the sound",
+    "A sound uploaded with freesound cli"
+  )
+  .option("--pack <name>", "The name of the sample pack")
+  .action(async (folder, options) => {
+    const absolutePath = resolve(folder);
+    const folderName = basename(absolutePath);
+    const pack = options.pack || folderName;
+    const files = await glob(`${absolutePath}/*.wav`);
+    // TODO: Check the pack doesn't already exist
+    const freesound = await login();
+    const tagsFromOptions = parseCommaSeparated(options.tags);
+    for (const file of files) {
+      const filepath = resolve(file);
+      const filename = basename(filepath);
+      let tags = [...tagsFromOptions, ...filenameTags(filename)];
+      tags = topUpTags(tags);
+
+      const stream = createReadStream(filepath);
+      console.log(`Uploading ${filename}`);
+      try {
+        await freesound.upload(stream, {
+          name: filename,
+          description: options.description,
+          tags,
+          pack,
+        });
+      } catch (err) {
+        console.error("There was a problem uploading", filename);
+      }
+    }
+  });
 
 export const upload = new Command()
   .name("upload")
@@ -40,4 +87,5 @@ export const upload = new Command()
         pack: options.pack,
       });
     }
-  });
+  })
+  .addCommand(uploadPack);
