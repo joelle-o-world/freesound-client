a979b58b2d2f37b544a81cfa3293b8fe30479d58
Joelle Plow <joelleplow@pm.me>
2022-08-02 13:44:30 +0100

    command: freesound sound open <sound-id>

--- src/commands/sound.ts
+++ src/commands/sound.ts
@@ -1,9 +1,21 @@
 import { Command } from "commander";
 import { play } from "./play";
 import { download } from "./download";
+import { download as nodeDownload } from "../node-download";
+import open from "open";
+
+export const openSound = new Command()
+  .name("open")
+  .description("Open a sound in the default application")
+  .argument("<sound-id>", "The ID of the sound you wish to open")
+  .action(async (soundId) => {
+    const soundPath = await nodeDownload(soundId);
+    open(soundPath);
+  });
 
 export const sound = new Command()
   .name("sound")
   .description("Commands about sounds")
   .addCommand(play)
-  .addCommand(download);
+  .addCommand(download)
+  .addCommand(openSound);
