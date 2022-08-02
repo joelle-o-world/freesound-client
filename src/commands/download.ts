c72c29cda7f50ff0dcedbc22e0cc35eaa53a716f
Joelle Plow <joelleplow@pm.me>
2022-08-02 11:29:21 +0100

    Downloading to specifc location

--- src/commands/download.ts
+++ src/commands/download.ts
@@ -1,12 +1,22 @@
 import { Command } from "commander";
 import { download as nodeDownload } from "../node-download";
+import fs from "fs/promises";
+import { extname } from "path";
 
 // TODO: --no-cache flag to disable caching downloads
 
 export const download = new Command()
   .name("download")
   .argument("<sound-id>", "ID of the sound to play")
+  .option("-o <output-file>")
   .description("Download a sample")
-  .action(async (soundId) => {
-    console.log(await nodeDownload(soundId));
+  .action(async (soundId, options) => {
+    if (options.o) {
+      const cachedPath = await nodeDownload(soundId);
+      if (extname(cachedPath) !== extname(options.o))
+        console.warn(`File extension does not match!`);
+      await fs.copyFile(cachedPath, options.o);
+    } else {
+      console.log(await nodeDownload(soundId));
+    }
   });
