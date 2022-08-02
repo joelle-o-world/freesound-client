da49b3419ab50954170b021a39d1d01c0ff3ff91
Joelle Plow <joelleplow@pm.me>
2022-08-02 10:02:55 +0100

    Uploading with descripion and tag options

--- src/commands/upload.ts
+++ src/commands/upload.ts
@@ -3,22 +3,66 @@ import path, { basename, resolve } from "path";
 import { createReadStream } from "fs";
 import { Command } from "commander";
 
+// TODO: --metadata <file> option
+
 export const upload = new Command()
   .name("upload")
   .argument("<files...>")
+  .option(
+    "--description <string>",
+    "Describe your the sound",
+    "A sound uploaded with freesound cli"
+  )
+  .option(
+    "-t|--tags <comma-separated-list>",
+    "Comma-separated-list of tags to attach to the uploaded sounds"
+  )
   .description("Upload sounds from your computer")
-  .action(async (files) => {
+  .action(async (files, options) => {
+    const tagsFromOptions = parseCommaSeparated(options.tags);
+
     for (const file of files) {
       const filepath = resolve(file);
       const filename = basename(filepath);
-      console.log("Trying to upload", filepath);
+
+      let tags = [...tagsFromOptions, ...filenameTags(filename)];
+      tags = topUpTags(tags);
+
       const stream = createReadStream(filepath);
       await (
         await login()
       ).upload(stream, {
         name: filename,
-        description: "A sound uploaded with freesound cli",
-        tags: [...path.parse(filename).name.split(/\W/)],
+        description: options.description,
+        tags,
       });
     }
   });
+
+function parseCommaSeparated(str: string | undefined) {
+  if (typeof str === "string") {
+    return str.split(/[,\s]+/);
+  } else return [];
+}
+
+/**
+ * Freesound imposes a minimum of 3 tags for every sound. This function tops
+ * the tags up if there are not enough.
+ */
+function topUpTags(tags: string[]) {
+  const minimumTags = 3;
+
+  const lastDitchTags = ["unlabeled", "unknown", "untitled"];
+  while (lastDitchTags.length > 0 && tags.length < minimumTags)
+    tags = [...tags, lastDitchTags.shift() as string];
+
+  return tags;
+}
+
+function isAnInterestingTag(tag: string) {
+  return tag.length > 1;
+}
+
+function filenameTags(filename: string) {
+  return path.parse(filename).name.split(/\W+/).filter(isAnInterestingTag);
+}
