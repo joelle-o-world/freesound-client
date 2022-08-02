dddc137ad4da48677eced66a233430cf9233e693
Joelle Plow <joelleplow@pm.me>
2022-08-02 09:21:18 +0100

    new option: freesound search --limit

--- src/commands/search.ts
+++ src/commands/search.ts
@@ -6,10 +6,12 @@ import { login } from "../connect";
 export const search = new Command()
   .name("search")
   .description("Search the freesound sample database")
+  .option("--limit <number>", "limit the number of search results", "20")
   .argument("<query>")
-  .action(async (query) => {
+  .action(async (query, options) => {
     // TODO: Output as a string
-    let limit = 20;
+
+    let limit: number = Number(options.limit);
     for await (const result of (await login()).search(query)) {
       console.log(`${result.id} - ${result.name}`);
       if (--limit == 0) break;
